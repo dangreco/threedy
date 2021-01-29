f95cb68db735d98369b06b885a9818b668bff5c9
Dan Greco <dan.greco@live.com>
2021-01-29 10:47:49 -0500

    Switch naming scheme from bundle.js to threedy-card.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'threedy-card.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
